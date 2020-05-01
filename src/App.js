97d01a28ada0e78bd082f347f476f56354b477f2
Jose <45631530+eljoseurena@users.noreply.github.com>
2020-05-01 15:47:16 -0400

    chore: migrate from CSS to SCSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Fragment>
+      <header>
+        <h1>Laura Sandoval</h1>
+        <nav>
+          <ul>
+            <li>Design</li>
+            <li>Photography</li>
+            <li>About</li>
+            <li>Get in Touch</li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <h2 class="visuallyhidden">Selected Works</h2>
+    </Fragment>
   );
 }
 
