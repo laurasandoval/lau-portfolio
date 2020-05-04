6c66a93d1252b554934ad97cfd9628fa68d3d235
Jose <45631530+eljoseurena@users.noreply.github.com>
2020-05-03 20:15:05 -0400

    chore(DesignWork): unify duplicate code

--- src/Containers/DesignWork/index.js
+++ src/Containers/DesignWork/index.js
@@ -3,57 +3,54 @@ import Data from "../../Assets/design-work.json";
 import Grid from "../../Components/Grid";
 import ProjectThumbnail from "../../Components/ProjectThumbnail";
 
-function DesignWork() {
-  
-  const randomizedDesignWork = Data.DesignWork.sort(() => Math.random() - Math.random());
-  const featuredProjects = randomizedDesignWork.filter(item => item.featured === true);
-  const firstFourFeaturedProjects = featuredProjects.slice(0, 4);
-  const remainingFeaturedProjects = featuredProjects.slice(4, featuredProjects.lenght);
-  const nonFeaturedProjects = Data.DesignWork.filter(item => item.featured === false);
-  const remainingProjects = nonFeaturedProjects.concat(remainingFeaturedProjects);
+class DesignWork extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Fragment>
-      <Grid featured>
-        {firstFourFeaturedProjects.map((project, index) => {
-          return (
-            <ProjectThumbnail
-              title={project.title}
-              client={project.client}
-              description={project.description}
-              start_year={project.start_year}
-              start_month={project.start_month}
-              end_year={project.end_year}
-              end_month={project.end_month}
-              featured={project.featured}
-              src={project.src}
-              thumbnails={project.thumbnails}
-              key={index}
-            />
-          );
-        })}
-      </Grid>
-      <Grid>
-        {remainingProjects.map((project, index) => {
-          return (
-            <ProjectThumbnail
-              title={project.title}
-              client={project.client}
-              description={project.description}
-              start_year={project.start_year}
-              start_month={project.start_month}
-              end_year={project.end_year}
-              end_month={project.end_month}
-              featured={project.featured}
-              src={project.src}
-              thumbnails={project.thumbnails}
-              key={index}
-            />
-          );
-        })}
-      </Grid>
-    </Fragment>
-  );
+    this._renderThumbnail = this._renderThumbnail.bind(this);
+  }
+
+  _renderThumbnail(project, index) {
+    return (
+      <ProjectThumbnail
+        title={project.title}
+        client={project.client}
+        description={project.description}
+        start_year={project.start_year}
+        start_month={project.start_month}
+        end_year={project.end_year}
+        end_month={project.end_month}
+        featured={project.featured}
+        src={project.src}
+        thumbnails={project.thumbnails}
+        key={index}
+      />
+    );
+  }
+
+  render() {
+    const randomizedDesignWork = Data.DesignWork.sort(() => Math.random() - Math.random());
+    const featuredProjects = randomizedDesignWork.filter(item => item.featured === true);
+    const firstFourFeaturedProjects = featuredProjects.slice(0, 4);
+    const remainingFeaturedProjects = featuredProjects.slice(4, featuredProjects.lenght);
+    const nonFeaturedProjects = Data.DesignWork.filter(item => item.featured === false);
+    const remainingProjects = nonFeaturedProjects.concat(remainingFeaturedProjects);
+
+    return (
+      <Fragment>
+        <Grid featured>
+          {firstFourFeaturedProjects.map((project, index) => {
+            return this._renderThumbnail(project, index);
+          })}
+        </Grid>
+        <Grid>
+          {remainingProjects.map((project, index) => {
+            return this._renderThumbnail(project, index);
+          })}
+        </Grid>
+      </Fragment>
+    );
+  }
 }
 
 export default DesignWork;
