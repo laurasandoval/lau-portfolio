c7b3a148611508b09e7ebcabe3e45d7e4609522b
Jose <45631530+eljoseurena@users.noreply.github.com>
2020-05-14 12:38:07 -0400

    chore: biased randomization attempts

--- src/Containers/DesignWork/index.js
+++ src/Containers/DesignWork/index.js
@@ -11,26 +11,68 @@ class DesignWork extends React.Component {
     super(props);
 
     this._renderThumbnail = this._renderThumbnail.bind(this);
+    this._randomGenerator = this._randomGenerator.bind(this);
+    this._sessionNumber = this._sessionNumber.bind(this);
+  }
+
+  _randomGenerator(seed) {
+      var m = 25;
+      var a = 11;
+      var c = 17;
+  
+      var z = seed;
+      return function() {
+        z = (a * z + c) % m;
+        return z/m;
+      }
   }
 
   _renderThumbnail(project, index) {
     return (
       <ProjectThumbnail
-        {...project}
-        as="article"
-        hover
-        key={index}
+      {...project}
+      as="article"
+      hover
+      key={index}
       />
-    );
-  }
+      );
+    }
+    
+    _sessionNumber() {
+      // if(sessionStorage.getItem("sessionSeed") === null) {
+      //   console.log("No sessionSeed in sessionStorage.");
+      //   sessionStorage.setItem("sessionSeed", Math.floor(Math.random() * 100) + 1);
+      //   console.log("Created sessionSeed in sessionStorage.");
+      //   console.log("sessionSeed: " + sessionStorage.getItem("sessionSeed"));
+      // } else {
+      //   console.log("A sessionSeed already exists in sessionStorage.");
+      //   console.log("sessionSeed: " + sessionStorage.getItem("sessionSeed"));
+      // }
+      
+      const sessionSeed = 1234;
+
+      return this._randomGenerator(sessionSeed);
+    }
 
   render() {
-    const randomizedDesignWork = Data.DesignWork.sort(() => Math.random() - Math.random());
-    const featuredProjects = randomizedDesignWork.filter(item => item.featured === true);
-    const firstFourFeaturedProjects = featuredProjects.slice(0, 4);
-    const remainingFeaturedProjects = featuredProjects.slice(4, featuredProjects.lenght);
-    const nonFeaturedProjects = Data.DesignWork.filter(item => item.featured === false);
-    const remainingProjects = nonFeaturedProjects.concat(remainingFeaturedProjects);
+        
+    const generator = this._sessionNumber();
+    const randomizedDesignWork = Data.DesignWork.sort(() => generator() - generator());
+
+    console.log("====== ORIGINAL ARRAY: ======");
+    console.log(Data.DesignWork);
+    
+    console.log("====== SHUFFLED ARRAY: ======");
+    console.log(randomizedDesignWork);
+
+    console.log("====== generator() - generator() RESULT: ======");
+    console.log(generator() - generator());
+    
+    // const featuredProjects = randomizedDesignWork.filter(item => item.featured === true);
+    // const firstFourFeaturedProjects = featuredProjects.slice(0, 4);
+    // const remainingFeaturedProjects = featuredProjects.slice(4, featuredProjects.lenght);
+    // const nonFeaturedProjects = Data.DesignWork.filter(item => item.featured === false);
+    // const remainingProjects = nonFeaturedProjects.concat(remainingFeaturedProjects);
 
     return (
       <Fragment>
@@ -40,13 +82,13 @@ class DesignWork extends React.Component {
         
         <GlobalHeader sticky />
         <AccessibilityLabel as="h2">Selected Works</AccessibilityLabel>
-        <Grid featured>
+        {/* <Grid featured>
           {firstFourFeaturedProjects.map((project, index) => {
             return this._renderThumbnail(project, index);
           })}
-        </Grid>
+        </Grid> */}
         <Grid>
-          {remainingProjects.map((project, index) => {
+          {Data.DesignWork.map((project, index) => {
             return this._renderThumbnail(project, index);
           })}
         </Grid>
