d1be1babd1218c524c1c282fc506ab0da305a213
Jose <45631530+eljoseurena@users.noreply.github.com>
2020-05-18 22:09:01 -0400

    chore(GlobalHeader): new design on desktop and simplified markup/CSS

--- src/Components/GlobalHeader/index.js
+++ src/Components/GlobalHeader/index.js
@@ -5,22 +5,36 @@ import "./index.scss";
 function GlobalHeader(props) {
   return (
     <header className="global-header" data-sticky={props.sticky}>
-      <div className="header-content-container">
-        <div className="header-content">
-          <h1>
-            <NavLink exact to="/">Laura Sandoval</NavLink>
-          </h1>
-          <nav>
-            <ul>
-              <li>
-                <NavLink activeClassName="active" exact to="/">Work</NavLink>
-              </li>
-              <li>
-                <NavLink activeClassName="active" exact to="/about">About</NavLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <div className="header-content">
+        <h1>
+          <NavLink exact to="/">
+            Laura Sandoval
+          </NavLink>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink activeClassName="active" exact to="/">
+                Design
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" exact to="/about">
+                Photography
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" exact to="/about">
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="active" exact to="/about">
+                Get in Touch
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
