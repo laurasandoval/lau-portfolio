268da1d16a9165e76d5afb8a59f5bbf78bf8fc29
Laura <45631530+laurasandoval@users.noreply.github.com>
2020-05-26 13:34:27 -0400

    chore(Accessibility): improve ProjectPageFallback accessibility for screen readers

--- src/Components/ProjectPageFallback/index.js
+++ src/Components/ProjectPageFallback/index.js
@@ -5,6 +5,7 @@ import "./index.scss";
 import GlobalHeader from "../GlobalHeader";
 import ProjectThumbnail from "../ProjectThumbnail";
 import Time from "../Time";
+import AccessibilityLabel from "../AccessibilityLabel";
 
 class ProjectPageFallback extends React.PureComponent {
   constructor(props) {
@@ -78,7 +79,7 @@ class ProjectPageFallback extends React.PureComponent {
           </div>
           <div className="project-info">
             <div className="main">
-              <h1 className="title">{this.props.title}</h1>
+              <h2 className="title">{this.props.title}</h2>
               <div className="description">
                 {this.props.description.map((p, i) => {
                   return <p key={i}>{p}</p>;
@@ -95,7 +96,10 @@ class ProjectPageFallback extends React.PureComponent {
                         target="_blank"
                         rel="noopener noreferrer"
                       >
-                        {cta.title}
+                        {cta.title}{" "}
+                        <span className="right-arrow" aria-hidden="true">
+                          →
+                        </span>
                       </a>
                     );
                   })}
@@ -104,27 +108,63 @@ class ProjectPageFallback extends React.PureComponent {
             </div>
             <div className="credits">
               {this.props.client && (
-                <div className="item">
-                  <h2>Client</h2>
+                <div className="item" role="text">
+                  <h3>
+                    Client<AccessibilityLabel>: </AccessibilityLabel>
+                  </h3>
                   <p>{this.props.client}</p>
                 </div>
               )}
-              <div className="item">
-                <h2>Release Date</h2>
+              <div className="item" role="text">
+                <h3>
+                  Release Date<AccessibilityLabel>: </AccessibilityLabel>
+                </h3>
                 <p>
                   <Time
                     year={this.props.release_year}
                     month={this.props.release_month}
+                    as="span"
                   />
                 </p>
               </div>
               {this.props.team &&
                 Object.keys(this.props.team).map((item, i) => {
                   return (
-                    <div className="item" key={i}>
-                      <h2>{item}</h2>
+                    <div className="item" key={i} role="text">
+                      <h3>
+                        {item}
+                        <AccessibilityLabel>: </AccessibilityLabel>
+                      </h3>
                       {this.props.team[item].map((person, i) => {
-                        return <p key={i}>{person.name}</p>;
+                        if (this.props.team[item].length > 1) {
+                          if (i + 1 === this.props.team[item].length) {
+                            return (
+                              <p key={i}>
+                                {person.name}
+                                <AccessibilityLabel>.</AccessibilityLabel>
+                              </p>
+                            );
+                          } else if (
+                            i + 1 ===
+                            this.props.team[item].length - 1
+                          ) {
+                            return (
+                              <p key={i}>
+                                {person.name}
+                                <AccessibilityLabel> and </AccessibilityLabel>
+                              </p>
+                            );
+                          } else {
+                            return (
+                              <p key={i}>
+                                {person.name}
+                                <AccessibilityLabel>, </AccessibilityLabel>
+                              </p>
+                            );
+                          }
+                        } else {
+                          return <p key={i}>{person.name}</p>;
+                        }
                       })}
                     </div>
                   );
