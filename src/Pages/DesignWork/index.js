785851570811c08b9d6564bd7d61d6b0a926f3b9
Lau <hi@lau.work>
2023-01-27 00:02:56 -0300

    refactor: remove old work & randomization engine

--- src/Pages/DesignWork/index.js
+++ src/Pages/DesignWork/index.js
@@ -5,7 +5,6 @@ import ProjectThumbnail from "../../Components/ProjectThumbnail";
 import AccessibilityLabel from "../../Components/AccessibilityLabel";
 import GlobalHeader from "../../Components/GlobalHeader";
 import { Helmet } from "react-helmet";
-import RemainingItems from "./RemainingItems";
 import Callout from "../../Components/Callout";
 
 class DesignWork extends React.PureComponent {
@@ -13,20 +12,6 @@ class DesignWork extends React.PureComponent {
     super(props);
 
     this._renderThumbnail = this._renderThumbnail.bind(this);
-    this._randomGenerator = this._randomGenerator.bind(this);
-    this._sessionNumber = this._sessionNumber.bind(this);
-  }
-
-  _randomGenerator(seed) {
-    var m = 25;
-    var a = 11;
-    var c = 17;
-
-    var z = seed;
-    return function () {
-      z = (a * z + c) % m;
-      return z / m;
-    };
   }
 
   _renderThumbnail(project, index, featured) {
@@ -43,30 +28,11 @@ class DesignWork extends React.PureComponent {
     );
   }
 
-  _sessionNumber() {
-    if (sessionStorage.getItem("sessionSeed") === null) {
-      sessionStorage.setItem(
-        "sessionSeed",
-        Math.floor(Math.random() * 1000) + 1
-      );
-    }
-
-    const sessionSeed = sessionStorage.getItem("sessionSeed");
-    return this._randomGenerator(sessionSeed);
-  }
-
   render() {
     const maxFeaturedCount = 6;
-    const maxRemainingCount = 8;
-    const generator = this._sessionNumber();
 
-    const randomizedDesignWork = Data.DesignWork.slice().sort(() => generator() - generator());
-    const featuredProjects = randomizedDesignWork.filter((item) => item.featured === true);
-    const featuredProjectsLimited = featuredProjects.slice(0, maxFeaturedCount);
-    const remainingFeaturedProjects = featuredProjects.slice(maxFeaturedCount,featuredProjects.lenght);
-    const nonFeaturedProjects = randomizedDesignWork.filter((item) => item.featured === false);
-    const remainingProjects = remainingFeaturedProjects.concat(nonFeaturedProjects);
-    const remainingProjectsLimited = remainingProjects.slice(0,maxRemainingCount);
+    const featuredProjects = Data.DesignWork.slice(0, maxFeaturedCount);
+    const remainingProjects = Data.DesignWork.slice(maxFeaturedCount, featuredProjects.lenght);
 
     return (
       <Fragment>
@@ -77,27 +43,14 @@ class DesignWork extends React.PureComponent {
         <GlobalHeader sticky />
         <AccessibilityLabel as="h2">Selected Works</AccessibilityLabel>
         <Grid featured>
-          {featuredProjectsLimited.map((project, index) => {
+          {featuredProjects.map((project, index) => {
             return this._renderThumbnail(project, index, true);
           })}
         </Grid>
         <Grid>
-          {remainingProjectsLimited.map((project, index) => {
+          {remainingProjects.map((project, index) => {
             return this._renderThumbnail(project, index);
           })}
-          <RemainingItems
-            itemsToShow={remainingProjects.slice(
-              maxRemainingCount,
-              remainingProjects.length
-            )}
-            as="article"
-            remainingCount={
-              remainingProjects.slice(
-                maxRemainingCount,
-                remainingProjects.length
-              ).length
-            }
-          />
         </Grid>
         <Callout>
           <p>This portfolio website uses Bézier Curves (aka. <a href="/squircles.png" rel="noopener noreferrer">squircles</a>) and it's also part of my portfolio. You can find the code on <a href="https://github.com/laurasandoval/lau-portfolio/" rel="noopener noreferrer">GitHub</a>.</p>
