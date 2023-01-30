c5ab31ae0cf8e89e9435eabacdfaa2b8ea9d2f78
Lau <hi@lau.work>
2023-01-30 18:09:18 -0300

    chore: add og meta tags

--- pages/projects/[...slug].js
+++ pages/projects/[...slug].js
@@ -26,8 +26,19 @@ export default function Project({ dataExport }) {
         <meta name="description" content={`${currentProject?.description[0]}`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="image" content={`/Work/${currentProject?.src}/${currentProject?.social_thumbnail}`} />
+
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/logo192.png" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={currentProject?.title} />
+        <meta property="og:description" content={`${currentProject?.description[0]}`} />
+        <meta property="og:image" content={`/Work/${currentProject?.src}/${currentProject?.social_thumbnail}`} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={currentProject?.title} />
+        <meta name="twitter:description" content={`${currentProject?.description[0]}`} />
+        <meta name="twitter:image" content={`/Work/${currentProject?.src}/${currentProject?.social_thumbnail}`} />
       </Head>
 
       <span>Hey! I'm a project!</span>
