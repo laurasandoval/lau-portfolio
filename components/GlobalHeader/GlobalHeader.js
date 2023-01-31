04509fdba4812e62005bc42ab94849690cd1a5d5
Lau <hi@lau.work>
2023-01-31 18:58:47 -0300

    fix: minor nav style fixes

--- components/GlobalHeader/GlobalHeader.js
+++ components/GlobalHeader/GlobalHeader.js
@@ -63,6 +63,7 @@ export default function GlobalHeader({
                         : "false"
                     : "false"
             }
+            data-nav-open={navOpen.toString()}
             style={{
                 "--background-color": backgroundColor
             }}
