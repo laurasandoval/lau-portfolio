87c649206826b515733a9e6070ad5b69996c0e02
Lau <hi@lau.work>
2023-01-31 17:20:55 -0300

    chore: add Vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '@/styles/globals.scss'
+import { Analytics } from '@vercel/analytics/react';
 import NextNProgress from 'nextjs-progressbar'
 
 export default function App({ Component, pageProps }) {
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }) {
     <>
       <NextNProgress color="#000" height={2} options={{ showSpinner: false }} />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
