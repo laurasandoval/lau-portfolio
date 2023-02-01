a5fc3b1f80b61039d4b377ba8cc53a0706b06314
Lau <hi@lau.work>
2023-02-01 00:33:26 -0300

    chore: use nextjs' suggested font import approach

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '@/styles/globals.scss'
 import { Analytics } from '@vercel/analytics/react';
+import { Inter } from '@next/font/google'
 import NextNProgress from 'nextjs-progressbar'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
       <NextNProgress color="#000" height={2} options={{ showSpinner: false }} />
       <Component {...pageProps} />
       <Analytics />
--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Inter:400,600"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
