c6ac96d9242263505488cb4c00eea01570e8e622
Lau <hi@lau.work>
2023-04-24 23:33:47 -0400

    chore: add more photography works

--- pages/api/photography-work.js
+++ pages/api/photography-work.js
@@ -1,16 +1,24 @@
 export default function handler(req, res) {
   res.status(200).json([
     {
-      "caption": "From the \"No peatones\" series, which captures a poignant critique of Santiago's emerging car-centric mega-infrastructures, emphasizing the erosion of public spaces as they morph into exclusive territories for vehicles, not people.",
+      "caption": "From the \"No peatones\" (No pedestrians) series, which captures a poignant critique of Santiago's emerging car-centric mega-infrastructures, emphasizing the erosion of public spaces as they morph into exclusive territories for vehicles, not people.",
       "period": "2019",
       "location": "Santiago, Chile",
       "images": [
         {
-          "src": "2019/no-peatones/160619_2.jpg",
+          "src": "no-peatones/160619_1.jpg",
           "alt": "",
         },
         {
-          "src": "2019/no-peatones/160619_1.jpg",
+          "src": "no-peatones/160619_2.jpg",
+          "alt": "",
+        },
+        {
+          "src": "no-peatones/160619_3.jpg",
+          "alt": "",
+        },
+        {
+          "src": "no-peatones/160619_4.jpg",
           "alt": "",
         },
       ]
@@ -21,22 +29,38 @@ export default function handler(req, res) {
       "location": "San Joaquín, Chile",
       "images": [
         {
-          "src": "2019/160419.jpg",
+          "src": "accidentally-wes-anderson/160419.jpg",
           "alt": "",
         },
       ]
     },
     {
-      "caption": "This series delves into everlasting struggles with belonging, exploring emotions and boundaries within one's own neighborhood, viewed from a distance, as an intimate questioning of identity and the concept of home.",
+      "caption": "This series delves into everlasting struggles with belonging, exploring emotions and boundaries within one's own city, viewed from a distance, as an intimate questioning of identity and the concept of home.",
       "period": "2017 — 2019",
       "location": "Santiago, Chile",
       "images": [
         {
-          "src": "2019/280319.jpg",
+          "src": "interpretaciones-de-lo-cotidiano/280319.jpg",
           "alt": "",
         },
         {
-          "src": "2017/170817.jpg",
+          "src": "interpretaciones-de-lo-cotidiano/280818.jpg",
+          "alt": "",
+        },
+        {
+          "src": "interpretaciones-de-lo-cotidiano/070519.jpg",
+          "alt": "",
+        },
+        {
+          "src": "interpretaciones-de-lo-cotidiano/170817.jpg",
+          "alt": "",
+        },
+        {
+          "src": "interpretaciones-de-lo-cotidiano/261118.jpg",
+          "alt": "",
+        },
+        {
+          "src": "interpretaciones-de-lo-cotidiano/040818.jpg",
           "alt": "",
         },
       ]
