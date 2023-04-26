0ae65ca1bb85596ac3ab1cf5529a974a69fada52
Lau <hi@lau.work>
2023-04-26 11:02:39 -0400

    fix: add image alt descriptions

--- pages/api/photography-work.js
+++ pages/api/photography-work.js
@@ -7,19 +7,19 @@ export default function handler(req, res) {
       "images": [
         {
           "src": "no-peatones/160619_1.jpg",
-          "alt": "",
+          "alt": "Black and white picture of a small child riding a miniature scooter, alone, through a massive highway. Noisy, low-contrast image.",
         },
         {
           "src": "no-peatones/160619_2.jpg",
-          "alt": "",
+          "alt": "Black and white picture of an unknown woman standing alone in the bottom-left third of the image, surrounded by massive highways. Cars and motorcycles passing by. Noisy, low-contrast image.",
         },
         {
           "src": "no-peatones/160619_3.jpg",
-          "alt": "",
+          "alt": "Black and white picture of a shared bicycle left alone in a small concrete island between car lanes. Noisy, low-contrast image.",
         },
         {
           "src": "no-peatones/160619_4.jpg",
-          "alt": "",
+          "alt": "Black and white picture of an empty car passage, with the front of the pedestrian road blocked by giant panels and concrete structures. Hanged on the panels, a sign reads \"Peatones cruzar por paso habilitado\". The framing of the image conveys that the sign is blocking pedestrians from crossing by.",
         },
       ]
     },
@@ -30,7 +30,7 @@ export default function handler(req, res) {
       "images": [
         {
           "src": "accidentally-wes-anderson/160419.jpg",
-          "alt": "",
+          "alt": "Bright, cozy and colorful image of an empty classroom. The bold, brick-colored chairs form an interesting texture in the bottom third of the image, contrasting with the light tones of the wooden wall at the back of the room. Competing with the intentional symmetry of the image, one of the chairs is slighty angled.",
         },
       ]
     },
@@ -41,27 +41,27 @@ export default function handler(req, res) {
       "images": [
         {
           "src": "interpretaciones-de-lo-cotidiano/280319.jpg",
-          "alt": "",
+          "alt": "Dark, black and white image of a nearly empty street with the sillouette of a man crossing in the middle. The slighly blurred light comming from the back of the street trees gives the image a dreamy look.",
         },
         {
           "src": "interpretaciones-de-lo-cotidiano/280818.jpg",
-          "alt": "",
+          "alt": "Low contrast, black and white image of a woman walking by the Mapocho river. The day is cloudy and the sidewalk is empty. The lighting of the image gives a nostalgic and melancholic feel.",
         },
         {
           "src": "interpretaciones-de-lo-cotidiano/070519.jpg",
-          "alt": "",
+          "alt": "Black and white image of a man waiting alone in a bus stop while cars pass by in the back. The cars are motion-blurred, and the man seems to be looking at the photographer suspicously.",
         },
         {
           "src": "interpretaciones-de-lo-cotidiano/170817.jpg",
-          "alt": "",
+          "alt": "Nostalgic, black and white image of a man walking his dog, seen from behind. The sillouette of the man contrasts heavily with the afternoon sun hitting the street (and the camera). The location of the image is very walkable and urban.",
         },
         {
           "src": "interpretaciones-de-lo-cotidiano/261118.jpg",
-          "alt": "",
+          "alt": "Black and white image of a crowd of pedestrians crossing the street outside of the crosswalk. The sillouettes of the pedestrians, which were captured from the back, contrasts heavily with the afternoon sun hitting the street (and the camera).",
         },
         {
           "src": "interpretaciones-de-lo-cotidiano/040818.jpg",
-          "alt": "",
+          "alt": "Nostalgic, black and white image of a man inside Santiago's subway. The picture is captured from a different subway car, and the small window of the man's car acts as an artificial frame for his body. The man's eyes are closed, and his body language seems to convey sadness, or tiredness.",
         },
       ]
     },
