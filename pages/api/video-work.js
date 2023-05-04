c47be71ed6e768619b420dd7369ef98bd336d238
Lau <hi@lau.work>
2023-05-03 21:56:51 -0400

    chore: add test videos

--- pages/api/video-work.js
+++ pages/api/video-work.js
@@ -6,9 +6,35 @@ export default function handler(req, res) {
       "location": "San Joaquín, Chile",
       "assets": [
         {
-          "src": "balance/abstract-card-intro.mp4",
+          "src": "test/test-1.mp4",
           "width": 1080,
           "height": 1920,
+          "alt": "",
+        },
+      ]
+    },
+    {
+      "caption": "video de balansdñflkhdce campaña blabla",
+      "period": "2022",
+      "location": "San Joaquín, Chile",
+      "assets": [
+        {
+          "src": "test/test-2.mp4",
+          "width": 1080,
+          "height": 1920,
+          "alt": "",
+        },
+      ]
+    },
+    {
+      "caption": "adsfñldjkhg AAAA",
+      "period": "2022",
+      "location": "San Joaquín, Chile",
+      "assets": [
+        {
+          "src": "test/test-3.mp4",
+          "width": 2160,
+          "height": 3840,
           "alt": "",
         },
       ]
