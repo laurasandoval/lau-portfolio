d422a11812b3ecb5bef5a34c93aea757eff086e7
Lau <hi@lau.work>
2023-05-04 13:13:12 -0400

    chore: add real video works

--- pages/api/video-work.js
+++ pages/api/video-work.js
@@ -1,40 +1,79 @@
 export default function handler(req, res) {
   res.status(200).json([
     {
-      "caption": "lorem ipsum dolor sit amet este es un caption de prueba",
+      "caption": "\"Abstract\" transit card design launch video for [Balance](/design/balance/app), the most positively reviewed app for managing Chilean transit cards in the App Store.",
       "period": "2022",
       "location": null,
       "assets": [
         {
-          "src": "test/test-1.mp4",
-          "width": 576,
-          "height": 1024,
+          "src": "balance/abstract-card-intro-es.mp4",
+          "width": 1080,
+          "height": 1920,
           "alt": "",
         },
       ]
     },
     {
-      "caption": "adsfñldjkhg AAAA probandooOOoOOOOO",
+      "caption": "Feature introduction video for \"[Ride with Balance](/design/balance/app)\", a new way to ride transit in Santiago de Chile.",
       "period": "2022",
       "location": null,
       "assets": [
         {
-          "src": "test/test-3.mp4",
-          "width": 1024,
-          "height": 576,
+          "src": "balance/ride-with-balance-launch-video-es.mp4",
+          "width": 1080,
+          "height": 1920,
           "alt": "",
         },
       ]
     },
     {
-      "caption": "esto no es un simulacro!!! es una prueba!!!!! tutuntss",
-      "period": "2022",
+      "caption": "Internal introduction video for the 2021 [Cornershop by Uber](/design/cornershop/head-of-customers-product-design)'s H1 Roadmap All Hands. The video highlights the biggest redesign to our order tracking page since the app's launch. The redesign was successfully implemented by the end of 2021.",
+      "period": "2021",
       "location": null,
       "assets": [
         {
-          "src": "test/test-2.mp4",
-          "width": 576,
-          "height": 1024,
+          "src": "cornershop-by-uber/order-timeline-introduction-video.mp4",
+          "width": 1920,
+          "height": 1080,
+          "alt": "",
+        },
+      ]
+    },
+    {
+      "caption": "Promotional video for my [Santiago Transit Map 2019](/design/santiago-transit-map/2019) design. Featured in Chile's national news network \"Televisión Nacional de Chile (TVN)\".",
+      "period": "2019",
+      "location": null,
+      "assets": [
+        {
+          "src": "santiago-transit-map/santiago-transit-map-2019-ad-en.mp4",
+          "width": 1920,
+          "height": 1080,
+          "alt": "",
+        },
+      ]
+    },
+    {
+      "caption": "\"Luces, no bocina\" (Lights, not horn) was one of a series of educational videos I drafted for [Kappo Bike](/design/kappo/kappo-brand-identity) in 2019. The ultimate goal was to use those videos as a part of an educational campaign for bicycle safety, targeted towards car drivers to promote safe interaction between bicycles and cars on the road.",
+      "period": "2019",
+      "location": null,
+      "assets": [
+        {
+          "src": "kappo/luces-no-bocina-kappo.mp4",
+          "width": 1920,
+          "height": 720,
+          "alt": "",
+        },
+      ]
+    },
+    {
+      "caption": "Promotional video for \"Saldo Red\", a now-defunct app (later replaced by [Balance](/design/balance/app)), highlighting the addition of Valparaíso's main transit card, \"MerVal\" to its list of supported transit cards.",
+      "period": "2019",
+      "location": null,
+      "assets": [
+        {
+          "src": "saldo-red/merval-saldo-red.mp4",
+          "width": 1920,
+          "height": 1920,
           "alt": "",
         },
       ]
