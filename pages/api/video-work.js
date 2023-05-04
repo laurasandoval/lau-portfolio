e1fdca3c4278dbb1be6bddc88fef44be490c797f
Lau <hi@lau.work>
2023-05-04 00:11:11 -0400

    chore: update test videos

--- pages/api/video-work.js
+++ pages/api/video-work.js
@@ -1,40 +1,40 @@
 export default function handler(req, res) {
   res.status(200).json([
     {
-      "caption": "video de balance campaña blabla",
+      "caption": "lorem ipsum dolor sit amet este es un caption de prueba",
       "period": "2022",
       "location": null,
       "assets": [
         {
           "src": "test/test-1.mp4",
-          "width": 1080,
-          "height": 1920,
+          "width": 576,
+          "height": 1024,
           "alt": "",
         },
       ]
     },
     {
-      "caption": "adsfñldjkhg AAAA",
+      "caption": "adsfñldjkhg AAAA probandooOOoOOOOO",
       "period": "2022",
       "location": null,
       "assets": [
         {
           "src": "test/test-3.mp4",
-          "width": 2160,
-          "height": 3840,
+          "width": 576,
+          "height": 1024,
           "alt": "",
         },
       ]
     },
     {
-      "caption": "video de balansdñflkhdce campaña blabla",
+      "caption": "esto no es un simulacro!!! es una prueba!!!!! tutuntss",
       "period": "2022",
       "location": null,
       "assets": [
         {
           "src": "test/test-2.mp4",
-          "width": 1080,
-          "height": 1920,
+          "width": 576,
+          "height": 1024,
           "alt": "",
         },
       ]
