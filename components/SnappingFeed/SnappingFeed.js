e82779e9d3bb334ef39fb46d6903d64032c0d83d
Lau <hi@lau.work>
2023-05-04 21:26:36 -0400

    chore: small details

--- components/SnappingFeed/SnappingFeed.js
+++ components/SnappingFeed/SnappingFeed.js
@@ -2,10 +2,19 @@ import './SnappingFeed.scss'
 import GlobalHeader from "./GlobalHeader/GlobalHeader";
 import Slide from "./Slide/Slide";
 import { NextSeo } from 'next-seo';
+import { useEffect } from 'react';
 
 function SnappingFeed({
     children,
 }) {
+    useEffect(() => {
+        document.body.classList.add("snapping_feed");
+
+        return () => {
+            document.body.classList.remove("snapping_feed");
+        }
+    }, [])
+
     return (
         <>
             <NextSeo
