da390778a1de8a6ad482867fe08ada69b3ce3038
Lau <hi@lau.work>
2023-11-10 10:21:09 -0300

    chore: project grid optimizations

--- components/ProjectsGrid/ProjectsGrid.js
+++ components/ProjectsGrid/ProjectsGrid.js
@@ -4,8 +4,7 @@ import { IconArrowDown } from '@tabler/icons-react';
 
 export default function ProjectsGrid({ featured, children }) {
     const gridRef = useRef(null);
-    // Start with all items if featured is true, otherwise start with 6.
-    const [itemsToShow, setItemsToShow] = useState(featured ? React.Children.count(children) : 6);
+    const [windowWidth, setWindowWidth] = useState(typeof window !== 'undefined' ? window.innerWidth : 0);
 
     const getComputedStyleColumnCount = () => {
         if (gridRef.current) {
@@ -14,42 +13,59 @@ export default function ProjectsGrid({ featured, children }) {
             const columnCount = columns.split(' ').length;
             return columnCount;
         }
-        return 0; // Default to 0 if we can't read the style
+        return 0;
     };
 
+    // Determine initial item count based on the column count
+    const getInitialItemCount = () => {
+        if (featured) return React.Children.count(children);
+        const columnCount = getComputedStyleColumnCount();
+        return columnCount === 1 ? 0 : 6;
+    };
+
+    const [itemsToShow, setItemsToShow] = useState(getInitialItemCount);
+
     const loadMoreItems = () => {
-        // Load more items only if featured is false
+        const columnCount = getComputedStyleColumnCount();
         if (!featured) {
-            setItemsToShow((prevItemsToShow) => prevItemsToShow + (2 * getComputedStyleColumnCount()));
+            const itemsToAdd = columnCount === 1 ? 4 : columnCount * 2;
+            setItemsToShow((prevItemsToShow) => prevItemsToShow + itemsToAdd);
         }
     };
 
     useEffect(() => {
-        // If featured is true, show all items and do not set up the resize listener
-        if (featured) {
-            setItemsToShow(React.Children.count(children));
+        // Ensure window is defined before using it
+        if (typeof window === 'undefined') {
             return;
         }
 
         const updateItemsToShow = () => {
             const columnCount = getComputedStyleColumnCount();
-            setItemsToShow(columnCount * 1);
+            if (featured) {
+                setItemsToShow(React.Children.count(children));
+            } else if (columnCount === 1) {
+                setItemsToShow(0);
+            } else {
+                setItemsToShow(columnCount * 1);
+            }
         };
 
         updateItemsToShow();
 
         const handleResize = () => {
-            updateItemsToShow();
+            const newWidth = window.innerWidth;
+            if (newWidth !== windowWidth) {
+                setWindowWidth(newWidth);
+                updateItemsToShow();
+            }
         };
 
         window.addEventListener('resize', handleResize);
 
         return () => window.removeEventListener('resize', handleResize);
-    }, [featured]); // Only re-run the effect if the featured prop changes
+    }, [featured, windowWidth]);
 
-    const displayedChildren = featured
-        ? children // Show all children if featured is true
-        : React.Children.toArray(children).slice(0, itemsToShow);
+    const displayedChildren = React.Children.toArray(children).slice(0, itemsToShow);
 
     return (
         <section ref={gridRef} className="projects_grid" data-featured={featured}>
