b1999855688a81458bc6dd7f9b5f50a6f5c731b4
Lau <hi@lau.work>
2024-08-18 14:21:14 -0400

    fix: correctly update sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,3 +1,5 @@
+import { getSortedPostsData } from "@/lib/posts";
+
 function generateSiteMap(designWorkData, server) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -5,21 +7,15 @@ function generateSiteMap(designWorkData, server) {
        <loc>${server}</loc>
      </url>
      <url>
-       <loc>${server}/about</loc>
-     </url>
-     <url>
-       <loc>${server}/resume</loc>
-     </url>
-     <url>
        <loc>${server}/photography</loc>
      </url>
      <url>
        <loc>${server}/videos</loc>
      </url>
-     ${designWorkData.map((project, index) => {
+     ${designWorkData.map((project) => {
     return `
        <url>
-           <loc>${`${server}/design/${project.src}`}</loc>
+           <loc>${`${server}/design/${project.id}`}</loc>
        </url>
      `;
   }).join('')}
@@ -28,26 +24,26 @@ function generateSiteMap(designWorkData, server) {
 }
 
 function SiteMap() {
-  // getServerSideProps will do the heavy lifting
+  // getServerSideProps will handle generating the sitemap
 }
 
 export async function getServerSideProps(context) {
-  const dev = process.env.NODE_ENV !== 'production'
-  const server = dev ? `http://localhost:3000` : `https://${context.req.headers.host}`
-  const url = `${server}/api/design-work`
-  const request = await fetch(url)
-  const designWorkData = await request.json()
+  const dev = process.env.NODE_ENV !== 'production';
+  const server = dev ? `http://localhost:3000` : `https://${context.req.headers.host}`;
 
-  // We generate the XML sitemap with the posts data
+  // Fetch and sort all posts data (similar to your other page)
+  const designWorkData = getSortedPostsData();
+
+  // Generate the XML sitemap with the project data
   const sitemap = generateSiteMap(designWorkData, server);
 
   context.res.setHeader('Content-Type', 'text/xml');
-  // we send the XML to the browser
+  // Send the XML to the browser
   context.res.write(sitemap);
   context.res.end();
 
   return {
-    props: {},
+    props: {}, // No props are needed as this page just serves the XML
   };
 }
 
