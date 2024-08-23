7a79395f6c158936b94074b2381f1ecef289516d
Lau <hi@lau.work>
2024-08-23 11:01:09 -0400

    feat: add support for embedding youtube videos into articles

--- components/ProjectArticleAsset/ProjectArticleAsset.js
+++ components/ProjectArticleAsset/ProjectArticleAsset.js
@@ -8,12 +8,16 @@ export default function ProjectArticleAsset({
     autoplay = true,
 }) {
     const [isPlaying, setIsPlaying] = useState(autoplay);
+    const [showVideo, setShowVideo] = useState(false);
     const videoRef = useRef(null);
 
     const isVideo = src.endsWith('.mp4') || src.endsWith('.webm');
+    const isYouTube = src.includes('youtube.com') || src.includes('youtu.be');
 
     const togglePlayPause = () => {
-        if (isPlaying) {
+        if (isYouTube) {
+            setShowVideo(true);
+        } else if (isPlaying) {
             videoRef.current.pause();
         } else {
             videoRef.current.play();
@@ -21,9 +25,44 @@ export default function ProjectArticleAsset({
         setIsPlaying(!isPlaying);
     };
 
+    const getYouTubeThumbnail = (url) => {
+        const videoId = url.includes('youtu.be')
+            ? url.split('/').pop()
+            : url.split('v=')[1].split('&')[0];
+        return `http://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+    };
+
     return (
         <figure className="project_article_asset">
-            {isVideo ? (
+            {isYouTube ? (
+                <div className="youtube_container">
+                    {!showVideo ? (
+                        <div className="youtube_thumbnail" onClick={togglePlayPause}>
+                            <img
+                                src={getYouTubeThumbnail(src)}
+                                alt={caption || "YouTube Thumbnail"}
+                                className="project_article_image"
+                            />
+                            <button className="play_button">
+                                <div className="play_button_affordance button">
+                                    <IconPlayerPlayFilled />
+                                </div>
+                            </button>
+                        </div>
+                    ) : (
+                        <iframe
+                            src={`https://www.youtube.com/embed/${src.includes('youtu.be')
+                                ? src.split('/').pop()
+                                : src.split('v=')[1].split('&')[0]
+                                }?autoplay=1`}
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                            className="youtube_iframe"
+                        />
+                    )}
+                </div>
+            ) : isVideo ? (
                 <div className="video_container">
                     <video
                         ref={videoRef}
@@ -36,7 +75,7 @@ export default function ProjectArticleAsset({
                     />
                     <button
                         onClick={togglePlayPause}
-                        className="playpause_button"
+                        className="playpause_button button"
                         title={isPlaying ? "Pause" : "Play"}
                     >
                         {isPlaying ? <IconPlayerPauseFilled /> : <IconPlayerPlayFilled />}
