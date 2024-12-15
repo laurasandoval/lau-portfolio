07a932eb22cc4dba3cc064d02a9f872930846b26
Lau <hi@lau.work>
2024-12-15 09:52:01 -0300

    refactor: add category links to project header

--- components/ProjectArticleHeader/ProjectArticleHeader.js
+++ components/ProjectArticleHeader/ProjectArticleHeader.js
@@ -1,21 +1,31 @@
 import Balancer from 'react-wrap-balancer'
 import { ProjectThumbnail } from '../ProjectThumbnail/ProjectThumbnail'
-import { formatYears, formatCategories } from '@/lib/formatters'
 import './ProjectArticleHeader.scss'
+import Link from 'next/link'
 
 export function ProjectArticleHeader({
+    peek = false,
     postData,
     autoPlayThumbnail = true,
 }) {
     return (
-        <div className="project_article_header">
+        <div className="project_article_header" data-peek={peek}>
             <div className="basic_info">
                 <h2 className="title">
                     <Balancer>
                         {postData.title}
                     </Balancer>
                 </h2>
-                <p className="period">{formatCategories(postData.workType)}</p>
+                <p className="subtitle">
+                    {postData.workType?.map((workType, i) => (
+                        <Link
+                            href={`/work/discipline/${workType.toLowerCase().replace(/[&]/g, '').replace(/\s+/g, '-')}`}
+                            key={i}
+                        >
+                            {workType}
+                        </Link>
+                    )).reduce((prev, curr) => [prev, ', ', curr])}
+                </p>
             </div>
             <p className="excerpt">
                 <Balancer>
@@ -24,7 +34,7 @@ export function ProjectArticleHeader({
             </p>
             <ProjectThumbnail
                 coverImage={postData.coverImage}
-                autoplay={true}
+                autoplay={autoPlayThumbnail}
                 img_only
                 placeholder={false}
             />
